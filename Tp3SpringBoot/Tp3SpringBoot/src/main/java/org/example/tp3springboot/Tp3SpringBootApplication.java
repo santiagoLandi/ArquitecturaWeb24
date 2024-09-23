e7d3bdb3dc7b7310bec77c9659a5feee789b98ea
Santiago Landi <slandi1992@gmail.com>
2024-09-23 11:06:05 -0300

    Se crea configuracion inicial del primer proyecto con Spring Boot

---
+++ Tp3SpringBoot/Tp3SpringBoot/src/main/java/org/example/tp3springboot/Tp3SpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.tp3springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp3SpringBootApplication.class, args);
+    }
+
+}
---
+++ Tp3SpringBoot/Tp3SpringBoot/src/test/java/org/example/tp3springboot/Tp3SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tp3springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
