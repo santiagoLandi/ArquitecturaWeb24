00d3a2b79f533f7b5b7b4af5b273f36162efc055
Santiago Landi <slandi1992@gmail.com>
2024-11-24 11:32:08 -0300

    se realizan cambios de orden en proyectos

--- MicroserviciosSpring/report-service/src/main/java/org/tudai/reportservice/dto/ScooterStatusReportDTO.java
+++
@@ -1,4 +0,0 @@
-package org.tudai.reportservice.dto;
-
-public class ScooterStatusReportDTO {
-}
--- MicroserviciosSpring/user-service/src/main/java/org/tudai/userservice/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package org.tudai.userservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.tudai.userservice.entity.Account;
-
-@Repository
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
